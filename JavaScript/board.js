21e149b6d40d62d73b899adb1905b02e7a642f0a
Haxrox <56497846+Haxrox@users.noreply.github.com>
2025-04-13 16:39:21 -0700

    Revert "Change gk charset to UTF-8 (#1)"
    This reverts commit a89121983e800d1e92b38036c5ae8899d411b87c.

--- JavaScript/board.js
+++ JavaScript/board.js
@@ -242,15 +242,15 @@ Board.prototype.postAddMove = function(mv, computerMove) {
     if (vlRep > -WIN_VALUE && vlRep < WIN_VALUE) {
       this.playSound("draw");
       this.result = RESULT_DRAW;
-      alertDelay("åŒæ–¹ä¸å˜ä½œå’Œï¼Œè¾›è‹¦äº†ï¼");
+      alertDelay("Ë«·½²»±ä×÷ºÍ£¬ÐÁ¿àÁË£¡");
     } else if (computerMove == (vlRep < 0)) {
       this.playSound("loss");
       this.result = RESULT_LOSS;
-      alertDelay("é•¿æ‰“ä½œè´Ÿï¼Œè¯·ä¸è¦æ°”é¦ï¼");
+      alertDelay("³¤´ò×÷¸º£¬Çë²»ÒªÆøÄÙ£¡");
     } else {
       this.playSound("win");
       this.result = RESULT_WIN;
-      alertDelay("é•¿æ‰“ä½œè´Ÿï¼Œç¥è´ºä½ å–å¾—èƒœåˆ©ï¼");
+      alertDelay("³¤´ò×÷¸º£¬×£ºØÄãÈ¡µÃÊ¤Àû£¡");
     }
     this.postAddMove2();
     this.busy = false;
@@ -268,7 +268,7 @@ Board.prototype.postAddMove = function(mv, computerMove) {
     if (!hasMaterial) {
       this.playSound("draw");
       this.result = RESULT_DRAW;
-      alertDelay("åŒæ–¹éƒ½æ²¡æœ‰è¿›æ”»æ£‹å­äº†ï¼Œè¾›è‹¦äº†ï¼");
+      alertDelay("Ë«·½¶¼Ã»ÓÐ½ø¹¥Æå×ÓÁË£¬ÐÁ¿àÁË£¡");
       this.postAddMove2();
       this.busy = false;
       return;
@@ -284,7 +284,7 @@ Board.prototype.postAddMove = function(mv, computerMove) {
     if (!captured) {
       this.playSound("draw");
       this.result = RESULT_DRAW;
-      alertDelay("è¶…è¿‡è‡ªç„¶é™ç€ä½œå’Œï¼Œè¾›è‹¦äº†ï¼");
+      alertDelay("³¬¹ý×ÔÈ»ÏÞ×Å×÷ºÍ£¬ÐÁ¿àÁË£¡");
       this.postAddMove2();
       this.busy = false;
       return;
@@ -310,7 +310,7 @@ Board.prototype.postAddMove2 = function() {
 }
 
 Board.prototype.postMate = function(computerMove) {
-  alertDelay(computerMove ? "è¯·å†æŽ¥å†åŽ‰ï¼" : "ç¥è´ºä½ å–å¾—èƒœåˆ©ï¼");
+  alertDelay(computerMove ? "ÇëÔÙ½ÓÔÙÀ÷£¡" : "×£ºØÄãÈ¡µÃÊ¤Àû£¡");
   this.postAddMove2();
   this.busy = false;
 }
